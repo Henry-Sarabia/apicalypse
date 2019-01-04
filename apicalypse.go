75a27d267be3d87fd0e2d91638a63d607d63bda9
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-01-03 21:28:18 -0800

    Refactor options encoding

--- apicalypse.go
+++ apicalypse.go
@@ -18,7 +18,8 @@ func URLRequest(method string, url string, options ...FuncOption) (*http.Request
 		return nil, errors.Wrap(err, "cannot create new options")
 	}
 
-	req, err := http.NewRequest(method, url+opt.encode(), nil)
+	url = url + opt.encode()
+	req, err := http.NewRequest(method, url, nil)
 	if err != nil {
 		return nil, errors.Wrapf(err, "cannot create request with method '%s' for url '%s'", method, url)
 	}
@@ -30,12 +31,16 @@ func URLRequest(method string, url string, options ...FuncOption) (*http.Request
 // The provided query options are written to the body of the request.
 // The default method is GET.
 func BodyRequest(method string, url string, options ...FuncOption) (*http.Request, error) {
+	if isBlank(url) {
+		return nil, ErrBlankArgument
+	}
+
 	opt, err := newOptions(options...)
 	if err != nil {
 		return nil, errors.Wrap(err, "cannot create new options")
 	}
 
-	req, err := http.NewRequest(method, url, opt.buffer())
+	req, err := http.NewRequest(method, url, opt.reader())
 	if err != nil {
 		return nil, errors.Wrapf(err, "cannot create request with method '%s' for url '%s'", method, url)
 	}

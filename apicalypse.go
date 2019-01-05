fdb614c604b2805470226a4ec284b338d2f4952b
Henry-Sarabia <henrysarabia@mail.fresnostate.edu>
2019-01-04 16:36:54 -0800

    Temporarily remove URLRequest

--- apicalypse.go
+++ apicalypse.go
@@ -5,28 +5,6 @@ import (
 	"net/http"
 )
 
-// URLRequest returns a request configured for the provided url using the provided method.
-// The provided query options are URL encoded and appended to the provided URL.
-// The default method is GET.
-func URLRequest(method string, url string, options ...FuncOption) (*http.Request, error) {
-	if isBlank(url) {
-		return nil, ErrBlankArgument
-	}
-
-	opt, err := newOptions(options...)
-	if err != nil {
-		return nil, errors.Wrap(err, "cannot create new options")
-	}
-
-	url = url + opt.encodedString()
-	req, err := http.NewRequest(method, url, nil)
-	if err != nil {
-		return nil, errors.Wrapf(err, "cannot create request with method '%s' for url '%s'", method, url)
-	}
-
-	return req, nil
-}
-
 // BodyRequest returns a request configured for the provided url using the provided method.
 // The provided query options are written to the body of the request.
 // The default method is GET.
